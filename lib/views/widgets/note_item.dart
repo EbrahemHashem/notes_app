079185821e97c3314499201facb9c76227b25efb
ebrahem hashem <ebrahemhashem72@gmail.com>
2024-05-23 09:14:40 +0300

    create edit view

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,61 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        right: 13,
-        left: 15,
-      ),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(250, 187, 178, 100),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 16,
-              ),
-              child: Text(
-                'build your career with me',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          right: 13,
+          left: 15,
+        ),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(250, 187, 178, 100),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(.6),
-                  fontSize: 20,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                ),
+                child: Text(
+                  'build your career with me',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.6),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  size: 35,
+                  color: Colors.black,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                size: 35,
-                color: Colors.black,
+            Text(
+              'May 21, 2022',
+              style: TextStyle(
+                color: Colors.black.withOpacity(.5),
+                fontSize: 16,
               ),
             ),
-          ),
-          Text(
-            'May 21, 2022',
-            style: TextStyle(
-              color: Colors.black.withOpacity(.5),
-              fontSize: 16,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
