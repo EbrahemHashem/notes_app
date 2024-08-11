da1191c5e47bea0b7b2027a0c001e5d8a7602460
Ebrahem <ebrahemhashem72@gmail.com>
2024-08-11 12:56:40 +0300

    create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,52 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.only(bottom: 8),
-        padding: const EdgeInsets.symmetric(
-          vertical: 18,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-          ListTile(
-            title: const Text(
-              'My Notes',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: Text(
-              'write your notes here to save it ',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.25),
-                fontSize: 20,
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(FontAwesomeIcons.trash),
-              color: Colors.black,
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 27),
-            child: Text(
-              '8August,2024',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.7),
+        );
+      },
+      child: Container(
+          margin: const EdgeInsets.only(bottom: 8),
+          padding: const EdgeInsets.symmetric(
+            vertical: 18,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+            ListTile(
+              title: const Text(
+                'My Notes',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                'write your notes here to save it ',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.25),
+                  fontSize: 20,
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(FontAwesomeIcons.trash),
+                color: Colors.black,
               ),
             ),
-          )
-        ]));
+            Padding(
+              padding: const EdgeInsets.only(right: 27),
+              child: Text(
+                '8August,2024',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.7),
+                ),
+              ),
+            )
+          ])),
+    );
   }
 }
