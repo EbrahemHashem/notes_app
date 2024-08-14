6eede388762a30a46e0338d26f5766458ebd2ea1
Ebrahem <ebrahemhashem72@gmail.com>
2024-08-14 10:35:39 +0300

    edit custom bottom sheet

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
-
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
+  final bool isLoading;
   final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
@@ -17,15 +17,19 @@ class CustomButton extends StatelessWidget {
           color: kprimaryColor,
           borderRadius: BorderRadius.circular(12),
         ),
-        child: const Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 22,
-            ),
-          ),
+        child: Center(
+          child: isLoading
+              ? const CircularProgressIndicator(
+                  color: Colors.black,
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
+                ),
         ),
       ),
     );
